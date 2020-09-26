8af2b63d91dda27937017c696629dd357a9055ca
joseamador0898 <jose.amador0898@gmail.com>
2020-09-25 22:44:42 -0500

    FINAL commit naive implementation

--- codeitsuisse/routes/saladSpree.py
+++ codeitsuisse/routes/saladSpree.py
@@ -39,32 +39,29 @@ def evaluateSaladSpree():
     logging.info("My result :{}".format(result))
     return json.dumps({'result' : min_sum})
 
-
-def minSumContiguous(arr, n, k): 
   
-    # k must be greater 
-    if (n < k): 
-      
-        print("Invalid") 
+def minSumContiguous(arr, n, k):
+    # k must be greater
+    if (n < k):
+        print("Invalid")
         return -1
-      
-    # Compute sum of first 
-    # window of size k 
+
+    # Compute sum of first
+    # window of size k
     res = 0
-    for i in range(k): 
-        res += arr[i] 
-  
-    # Compute sums of remaining windows by 
-    # removing first element of previous 
-    # window and adding last element of  
-    # current window. 
-    curr_sum = res 
-    for i in range(k, n): 
-      
-        curr_sum += arr[i] - arr[i-k] 
-        res =  min(res, curr_sum) 
-  
-    return res 
+    for i in range(k):
+        res += int(arr[i])
+
+        # Compute sums of remaining windows by
+    # removing first element of previous
+    # window and adding last element of
+    # current window.
+    curr_sum = res
+    for i in range(k, n):
+        curr_sum += arr[i] - arr[i - k]
+        res = min(res, curr_sum)
+
+    return res
 
 
 
