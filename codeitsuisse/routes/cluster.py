9fb45adbc1fc3dee6ceb4b6b518e768e1481183a
joseamador0898 <jose.amador0898@gmail.com>
2020-09-26 22:57:34 -0500

    Fix final

--- codeitsuisse/routes/cluster.py
+++ codeitsuisse/routes/cluster.py
@@ -22,28 +22,19 @@ def evaluateCluster():
     return jsonify({"answer": result})
 
 class clusterSolution:
-    def dfs(self, grid, i, j):
-        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] == '*':
+    def dfs(self, grid, i, j,prev):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != ('1' or '0') or prev == '0':
             return
-        temp = grid[i][j]
         grid[i][j] = '#'
-        if(grid[i][j+1] == '1' or (temp == 1 and grid[i][j+1] == 0)):
-            self.dfs(grid, i, j+1)
-        if(grid[i+1][j+1] == '1' or (temp == 1 and grid[i+1][j+1] == 0)):
-            self.dfs(grid, i+1, j+1)
-        if(grid[i+1][j] == '1' or (temp == 1 and grid[i+1][j] == 0)):
-            self.dfs(grid, i+1, j)
-        if(grid[i+1][j-1] == '1' or (temp == 1 and grid[i+1][j-1] == 0)):
-            self.dfs(grid, i+1, j-1)
-        if(grid[i][j-1] == '1' or (temp == 1 and grid[i][j-1] == 0)):
-            self.dfs(grid, i, j-1)
-        if(grid[i-1][j-1] == '1' or (temp == 1 and grid[i-1][j-1] == 0)):
-            self.dfs(grid, i-1, j-1)
-        if(grid[i-1][j] == '1' or (temp == 1 and grid[i-1][j] == 0)):
-            self.dfs(grid, i-1, j)
-        if(grid[i-1][j+1] == '1' or (temp == 1 and grid[i-1][j+1] == 0)):
-            self.dfs(grid, i-1, j+1)
-    
+        self.dfs(grid, i, j+1,grid[i][j])
+        self.dfs(grid, i+1, j+1,grid[i][j])
+        self.dfs(grid, i+1, j,grid[i][j])
+        self.dfs(grid, i+1, j-1,grid[i][j])
+        self.dfs(grid, i, j-1,grid[i][j])
+        self.dfs(grid, i-1, j-1,grid[i][j])
+        self.dfs(grid, i-1, j,grid[i][j])
+        self.dfs(grid, i-1, j+1,grid[i][j])
+
     def numClusters(self, grid):
         if not grid:
             return 0
@@ -52,7 +43,7 @@ class clusterSolution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    self.dfs(grid, i, j)
+                    self.dfs(grid, i, j,grid[i][j])
                     count += 1
         return count
 
